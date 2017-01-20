57282fbaa0efa520eb18da86efab317275f2a90e
TechRunner2 <techrunner2.0@gmail.com>
2017-01-20 10:02:45 -0600

    Added some blocks and a dependency

--- init.lua
+++ init.lua
@@ -1,34 +1,32 @@
-aether-2_minetest = {}
+aether2_minetest = {}
 
 --Load File
- dofile(minetest.get_modpath("aether-2_minetest") .. "/config.txt")
+ dofile(minetest.get_modpath("aether2_minetest") .. "/config.txt")
 
 if run_ABM then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/abm.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/abm.lua')
 end
 if enable_mobs then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/mobs.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/mobs.lua')
 end
 if enable_crafting then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/crafting.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/crafting.lua')
 end
 if enable_nodes then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/nodes.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/nodes.lua')
 end
 if enable_cooking then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/cooking.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/cooking.lua')
 end
 if enable_items then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/craft_items.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/craft_items.lua')
 end
 if enable_fuels then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/fuels.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/fuels.lua')
 end
 if enable_tools then
-	dofile(minetest.get_modpath('aether-2_minetest')..'/tools.lua')
+	dofile(minetest.get_modpath('aether2_minetest')..'/tools.lua')
 end
 if enable_functions then
-		dofile(minetest.get_modpath('aether-2_minetest')..'/functions.lua')
+		dofile(minetest.get_modpath('aether2_minetest')..'/functions.lua')
 end
-
-
--- nodes.lua
+++ nodes.lua
@@ -1,23 +1,15 @@
 -- Groups = crumbly =dirt, cracky = stone, snappy = leaves, choppy = wood, explody, oddly_breakable_by_hand
 
 --Dirts
-minetest.register_node("aether-2_minetest:aether_dirt"), {
+minetest.register_node("aether2_minetest:aether_dirt", {
   description = 'Aether Dirt',
   tiles = {"aether_dirt"},
   groups = {crumbly = 1},
   sounds = default.node_sound_dirt_defaults(),
 
-}
+})
 
-minetset.register_node("aether-2_minetest:aether_dirt_with_grass"), {
-  description = 'Aether Grass',
-  tiles = {"aether_dirt_with_grass"},
-  groups = {crumbly=1},
-  drop = 'aether-2_minetest:aether_dirt',
-  sounds = default.node_sound_dirt_defaults(),
-
-}
-minetest.register_node("aether-2_minetest:aether_dirt_with_grass", {
+minetest.register_node("aether2_minetest:aether_dirt_with_grass", {
 	description = "Aether Grass",
 	tiles = {"aether_grass.png", "aether_dirt.png",
 		{name = "aether_dirt.png^aether_grass_side.png",
@@ -31,6 +23,66 @@ minetest.register_node("aether-2_minetest:aether_dirt_with_grass", {
 
 
 --Stones
-
+minetest.register_node("aether2_minetest:hollystone", {
+	description = "Hollystone",
+	tiles = {"holystone.png"},
+	groups = {cracky = 3},
+	drop = 'default:coal_lump',
+	sounds = default.node_sound_stone_defaults(),
+})
 
 --Trees
+
+minetest.register_node("aether2_minetest:skyroot_logs", {
+	description = "skyroot_logs",
+	tiles = {"skyroot_logs_top.png", "skyroot_logs_top.png", "skyroot_logs.png"},
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	groups = {tree = 1, choppy = 2,},
+	sounds = default.node_sound_wood_defaults(),
+
+	on_place = minetest.rotate_node
+})
+
+minetest.register_node("aether2_minetest:golden_oak_logs", {
+	description = "Golden Oak Logs",
+	tiles = {"golden_oak_logs_top.png", "golden_oak_logs_top.png", "golden_oak_logs.png"},
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	groups = {tree = 1, choppy = 2,},
+	sounds = default.node_sound_wood_defaults(),
+  drop = {
+
+  }
+	on_place = minetest.rotate_node
+})
+
+minetest.register_node("aether2_minetest:skyroot_leaves", {
+  description = "Skyroot Leaves",
+  drawtype = "allfaces_optional",
+  waving = 1,
+  tiles = {"skyroot_leaves.png"},
+  special_tiles = {"skyroot_leaves_simple.png"},
+  paramtype = "light",
+  is_ground_content = false,
+  groups = {snappy = 3, leafdecay = 3, leaves = 1},
+  drop = {
+    max_items = 1,
+    items = {
+      {
+        -- player will get sapling with 1/20 chance
+        items = {'aether2_minetest:skyroot_sapling'},
+        rarity = 20,
+      },
+      {
+        -- player will get leaves only if he get no saplings,
+        -- this is because max_items is 1
+        items = {'default:leaves'},
+      }
+    }
+  },
+  sounds = default.node_sound_leaves_defaults(),
+
+  after_place_node = default.after_place_leaves,
+})
+})
