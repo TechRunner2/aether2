c5d72fd73e94547db5a77b4be42d9570023cc44b
TechRunner2 <techrunner2.0@gmail.com>
2017-01-23 11:59:41 -0600

    Added Aliases

---
+++ aliases.lua
@@ -0,0 +1,4 @@
+minetest.register_alias("aether_dirt","aether2_minetest:aether_dirt")
+minetest.register_alias("hollystone","aether2_minetest:hollystone")
+minetest.register_alias("skyroot","aether2_minetest:skyroot_logs")
+minetest.register_alias("golden_oak","aether2_minetest:golden_oak_logs")
