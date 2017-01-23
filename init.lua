bfe9011e7297eabaf3c14f1dbf8d44591532cb29
TechRunner2 <techrunner2.0@gmail.com>
2017-01-23 13:24:26 -0600

    removed config file

--- init.lua
+++ init.lua
@@ -3,31 +3,12 @@ local default_path = minetest.get_modpath("aether2_minetest")
 --Load File
 dofile(minetest.get_modpath("aether2_minetest") .. "/config.txt")
 dofile(minetest.get_modpath("aether2_minetest") .. "/aliases.lua")
-
-if run_ABM == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/abm.lua')
-end
-if enable_mobs == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/mobs.lua')
-end
-if enable_crafting == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/crafting.lua')
-end
-if enable_nodes == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/nodes.lua')
-end
-if enable_cooking == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/cooking.lua')
-end
-if enable_items == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/craft_items.lua')
-end
-if enable_fuels == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/fuels.lua')
-end
-if enable_tools == true then
-	dofile(minetest.get_modpath('aether2_minetest')..'/tools.lua')
-end
-if enable_functions == true then
-		dofile(minetest.get_modpath('aether2_minetest')..'/functions.lua')
-end
+dofile(minetest.get_modpath('aether2_minetest')..'/abm.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/mobs.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/crafting.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/nodes.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/cooking.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/craft_items.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/fuels.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/tools.lua')
+dofile(minetest.get_modpath('aether2_minetest')..'/functions.lua')
